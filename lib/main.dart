c3d042746f7e4668a2a31471cc6bd9e2e341a99a
Douglas Spicer <doug@Douglass-MacBook-Air.local>
2024-08-26 15:34:56 -0600

    Updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:rdp_to_do_list/screens/home_page.dart';
 import 'firebase_options.dart';
-
 
 void main() async {
   runApp(const MainApp());
@@ -23,12 +23,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('we got a simpur Hellerr Werld!!'),
-        ),
+    return MaterialApp(
+      title: 'To-do list',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const HomePage(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,8 +11,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
 
+  final List<String> tasks = <String>[];
+  final List<bool> checkBoxes = List.generate(8, (index) => false);
+  TextEditingController nameController = TextEditingController();
+
+  void addItemToList() async {
+    final String taskName = nameController.text;
+
+    await db.collection('tasks').add({
+      'name': taskName,
+      'completed': false,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+
+    setState(() {
+      tasks.insert(0, taskName);
+    });
+  }
+
+  void removeItems(int index) async {
+    // Get the task to be removed
+    String taskToBeRemoved = tasks[index];
+
+    // Remove the task from Firestore
+    QuerySnapshot querySnapshot = await db
+        .collection('tasks')
+        .where('name', isEqualTo: taskToBeRemoved)
+        .get();
+
+    if (querySnapshot.size > 0) {
+      DocumentSnapshot documentSnapshot = querySnapshot.docs[0];
+
+      // Delete the document from Firestore
+      await db.collection('tasks').doc(documentSnapshot.id).delete();
+
+      // Update the state after deletion
+      setState(() {
+        tasks.removeAt(index);
+        checkBoxes.removeAt(index);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
